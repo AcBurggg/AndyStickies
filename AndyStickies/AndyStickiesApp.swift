cf4921cf26b6360c0821bccaa9ca578a5ea17b9c
AcBurggg <andrewburgessgt@gmail.com>
2025-06-09 16:39:15 -0400

    Initial Commit

---
+++ AndyStickies/AndyStickiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndyStickiesApp.swift
+//  AndyStickies
+//
+//  Created by Andrew Burgess on 6/9/25.
+//
+
+import SwiftUI
+
+@main
+struct AndyStickiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AndyStickiesTests/AndyStickiesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AndyStickiesTests.swift
+//  AndyStickiesTests
+//
+//  Created by Andrew Burgess on 6/9/25.
+//
+
+import Testing
+@testable import AndyStickies
+
+struct AndyStickiesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
