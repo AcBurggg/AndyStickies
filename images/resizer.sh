1e6ef385a445d15f478e117cd47797143e543607
AcBurggg <andrewburgessgt@gmail.com>
2025-06-16 18:30:07 -0400

    done.

--- images/resizer.sh
+++ images/resizer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#used brew imagemagick to get squircle shaped transparent corner icon
 for size in 16 32 64 128 256 512 1024; do
-  sips -z $size $size icon.jpg --out AppIcon-${size}x${size}.png
+  sips -z $size $size rounded_icon.png --out AppIcon-${size}x${size}.png
 done
